914201e26b65f35e4f396b628ec9c065e2914569
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-05 15:23:01 +0530

    add:MongoDb database connection

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 // Utils
+import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+connectDB(process.env.MONGO_URI);
 
 const app = express();
 app.use(express.json());
