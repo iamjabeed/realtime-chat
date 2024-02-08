a5cc8e8eb8837ce26d18dbded88aa4b363111d07
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-08 21:44:10 +0530

    feat: protected routes (client side)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import Login from "./pages/auth/Login.jsx";
 import SignUp from "./pages/auth/Signup.jsx";
 import Home from "./pages/home/Home.jsx";
+import { useAuthContext } from "./context/AuthContext.jsx";
 
 function App() {
+  const { authUser } = useAuthContext();
   return (
     <div className="p-4 h-screen flex items-center justify-center">
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
       </Routes>
       <Toaster />
     </div>
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,7 +1,9 @@
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import { useAuthContext } from "../context/AuthContext";
 const useSignup = () => {
   const [loading, setLoading] = useState(false);
+  const { setAuthUser } = useAuthContext();
 
   const signup = async ({
     fullName,
@@ -39,6 +41,10 @@ const useSignup = () => {
       if (data?.error) {
         throw new Error(data?.error);
       }
+      // Set user information to local storage
+      localStorage.setItem("chat-user", JSON.stringify(data));
+      // Update AuthUser state in the context
+      setAuthUser(data);
     } catch (error) {
       toast.error(error?.message);
     } finally {
