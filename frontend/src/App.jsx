d0ee0d0a9dee580cd6f85b27c6f4319a6b7a0f05
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-08 17:16:39 +0530

    feat: register and login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
+import Login from "./pages/auth/Login.jsx";
+import SignUp from "./pages/auth/Signup.jsx";
+
 function App() {
   return (
-    <>
-      <h1 className="">Chat app</h1>
-    </>
+    <div className="h-screen bg-gray-400">
+      <h1 className="flex justify-center items-center text-2xl py-4 text-red-700 font-bold">
+        Chat app
+      </h1>
+      <SignUp />
+    </div>
   );
 }
 
