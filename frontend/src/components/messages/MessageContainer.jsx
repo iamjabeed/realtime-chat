5bc3b7fa2f411e339ba1223ff753f8449babfe4c
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-09 12:05:37 +0530

    style: message container

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,29 @@
+import { useEffect } from "react";
 import { TiMessages } from "react-icons/ti";
+
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
-import { useState } from "react";
+import useConversation from "../../zustand/useConversation";
+import { useAuthContext } from ".././../context/AuthContext";
 
 const MessageContainer = () => {
-  const [noChatSelected, setNoChatSelected] = useState(true);
+  const { selectedConversation, setSelectedConversation } = useConversation();
+
+  useEffect(() => {
+    return () => setSelectedConversation(null);
+  }, []);
   return (
     <div className="px-4 md:min-w-[450px] flex flex-col">
-      {noChatSelected ? (
+      {!selectedConversation ? (
         <NoChatSelected />
       ) : (
         <>
           {/* Header */}
           <div className="bg-slate-500 px-4 py-2 mb-2">
             <span className="label-text">To:</span>{" "}
-            <span className="text-gray-900 font-bold">John doe</span>
+            <span className="text-gray-900 font-bold">
+              {selectedConversation?.fullName}
+            </span>
           </div>
 
           <Messages />
@@ -27,11 +36,12 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
-  // const { authUser } = useAuthContext();
+  const { authUser } = useAuthContext();
+
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 Jabeed ❄</p>
+        <p>Welcome 👋 {authUser?.fullName} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
