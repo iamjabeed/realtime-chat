005bc1f37cfcc296c8ab5e985cf15c1d5c38da77
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-09 10:29:36 +0530

    logout integration

--- frontend/src/components/sidebar/LogoutBtn.jsx
+++ frontend/src/components/sidebar/LogoutBtn.jsx
@@ -1,8 +1,17 @@
 import { BiLogOut } from "react-icons/bi";
+import useLogout from "../../hooks/useLogout";
 const LogoutBtn = () => {
+  const { loading, logout } = useLogout();
   return (
-    <div className="mt-4 mx-4">
-      <BiLogOut className="w-6 h-6 cursor-pointer" size={24} />
+    <div className="mt-auto">
+      {!loading ? (
+        <BiLogOut
+          className="w-6 h-6 text-white cursor-pointer"
+          onClick={logout}
+        />
+      ) : (
+        <span className="loading loading-spinner"></span>
+      )}
     </div>
   );
 };
--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -102,8 +102,15 @@ const SignUp = () => {
           </Link>
 
           <div>
-            <button className="btn btn-block btn-sm mt-2 border border-slate-700">
-              Sign Up
+            <button
+              className="btn btn-block btn-sm mt-2 border border-slate-700"
+              disabled={loading}
+            >
+              {loading ? (
+                <span className="loading loading-spinner"></span>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
