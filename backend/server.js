8eb0c31a274e915efddf56b5022f55eef9fd7db5
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-10 17:40:04 +0530

    socket server setup

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 // Utils
+import { app, server } from "./socket/socket.js";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import userRoutes from "./routes/user.route.js";
 
-const app = express();
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
@@ -23,4 +23,4 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
