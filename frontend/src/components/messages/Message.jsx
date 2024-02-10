1e804bdbe397118c615e1052606f1bbd6c1edc09
Jabeed Syed <smartyjabeedbhai001@gmail.com>
2024-02-10 17:14:13 +0530

    feat: sending messages

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,22 +1,47 @@
 import { RxAvatar } from "react-icons/rx";
-const Message = () => {
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../zustand/useConversation";
+const Message = ({ message }) => {
+  const { authUser } = useAuthContext();
+  const { selectedConversation } = useConversation();
+
+  const fromMe = message.senderId === authUser._id;
+
+  const chatClassName = fromMe ? "chat-end" : "chat-start";
+  const profilePic = fromMe
+    ? authUser.profilePic
+    : selectedConversation?.profilePic;
+  const bubbleBgColor = fromMe ? "bg-blue-500" : "";
+  const shakeClass = message.shouldShake ? "shake" : "";
+
+  function extractTime(dateString) {
+    const date = new Date(dateString);
+    const hours = padZero(date.getHours());
+    const minutes = padZero(date.getMinutes());
+    return `${hours}:${minutes}`;
+  }
+
+  // Helper function to pad single-digit numbers with a leading zero
+  function padZero(number) {
+    return number.toString().padStart(2, "0");
+  }
+  const formattedTime = extractTime(message.createdAt);
+
   return (
-    <div className="chat chat-end">
+    <div className={`chat ${chatClassName}`}>
       <div className="chat-image avatar">
         <div className="w-10 rounded-full">
-          {/* <img
-            src="https://cdn0.iconfinder.com/data/icons/communication-line-10/24/account_profile_user_contact_person_avatar_placeholder-512.png"
-            alt="user avatar"
-            className="text-white"
-          /> */}
-          <RxAvatar size={24} />
+          <img alt="user avatar" src={profilePic} />
+          {/* <RxAvatar size={24} /> */}
         </div>
       </div>
-      <div className={`chat-bubble text-white bg-blue-500`}>
-        Hi How you doin?
+      <div
+        className={`chat-bubble text-white ${bubbleBgColor} ${shakeClass} pb-2`}
+      >
+        {message.message}
       </div>
       <div className="chat-footer opacity-50 text-xs flex gap-1 items-center">
-        12:04
+        {formattedTime}
       </div>
     </div>
   );
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,19 +1,24 @@
+import useGetMessages from "../../hooks/useGetMessages.js";
 import Message from "./Message.jsx";
+import MessageSkeleton from "../shimmerUi/MessageSkeleton.jsx";
 
 const Messages = () => {
+  const { loading, messages } = useGetMessages();
+  // console.log(messages);
   return (
     <div className="px-4 flex-1 overflow-auto">
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+      {loading &&
+        [...Array(3)].map((_, index) => <MessageSkeleton key={index} />)}
+
+      {!loading && messages.length === 0 && (
+        <p className="text-center">Send a message to start the conversation</p>
+      )}
+
+      {!loading &&
+        messages?.length > 0 &&
+        messages.map((message) => (
+          <Message key={message._id} message={message} />
+        ))}
     </div>
   );
 };
